e8474a77bb5627043e0744863f0825aa4d9b3d24
aleonoff <103482999+aleonoff@users.noreply.github.com>
2024-10-24 19:43:56 +0300

    Add file currencies.php in language
    for module currencies

---
+++ upload/admin/language/en-gb/extension/module/currencies.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']      = 'Currencies';
+
+// Text
+$_['text_extension']     = 'Extensions';
+$_['text_success']       = 'Success: You have modified currencies details!';
+$_['text_edit']          = 'Edit Currencies';
+$_['text_yes']           = 'Yes';
+$_['text_no']            = 'No';
+
+// Entry
+$_['entry_currencies_cache']  = 'Cache the result of an API request?';
+$_['entry_status']            = 'Status';
+
+// Error 
+$_['error_permission']   = 'Warning: You do not have permission to modify module Currencies!';
