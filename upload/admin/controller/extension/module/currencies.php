56de686fbdecd781548c69d10be1e3b6e370b625
aleonoff <103482999+aleonoff@users.noreply.github.com>
2024-10-24 19:41:03 +0300

    Update currencies.php

--- upload/admin/controller/extension/module/currencies.php
+++ upload/admin/controller/extension/module/currencies.php
@@ -18,8 +18,8 @@ class ControllerExtensionModuleCurrencies extends Controller {
 		$data['text_edit'] = $this->language->get('text_edit');
 		$data['text_enabled'] = $this->language->get('text_enabled');
 		$data['text_disabled'] = $this->language->get('text_disabled');
-        $data['text_yes'] = $this->language->get('text_yes');
-        $data['text_no'] = $this->language->get('text_no');
+        	$data['text_yes'] = $this->language->get('text_yes');
+        	$data['text_no'] = $this->language->get('text_no');
 
 		$data['entry_currencies_cache'] = $this->language->get('entry_currencies_cache');
 		$data['entry_cache_time'] = $this->language->get('entry_cache_time');
@@ -54,8 +54,8 @@ class ControllerExtensionModuleCurrencies extends Controller {
 		$data['action'] = $this->url->link('extension/module/currencies', 'token=' . $this->session->data['token'], true);
 		$data['cancel'] = $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=payment', true);
 
-        $data['currencies_cache'] = isset($this->request->post['currencies_cache']) ? (int)$this->request->post['currencies_cache'] : (int)$this->config->get('currencies_cache');
-        $data['currencies_status'] = isset($this->request->post['currencies_status']) ? (int)$this->request->post['currencies_status'] : (int)$this->config->get('currencies_status');
+        	$data['currencies_cache'] = isset($this->request->post['currencies_cache']) ? (int)$this->request->post['currencies_cache'] : (int)$this->config->get('currencies_cache');
+        	$data['currencies_status'] = isset($this->request->post['currencies_status']) ? (int)$this->request->post['currencies_status'] : (int)$this->config->get('currencies_status');
 
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
@@ -64,12 +64,12 @@ class ControllerExtensionModuleCurrencies extends Controller {
 		$this->response->setOutput($this->load->view('extension/module/currencies', $data));
 	}
 
-    protected function validate() {
-        if (!$this->user->hasPermission('modify', 'extension/payment/bank_transfer')) {
-            $this->error['warning'] = $this->language->get('error_permission');
-        }
-
-        return !$this->error;
-    }
+	protected function validate() {
+		if (!$this->user->hasPermission('modify', 'extension/payment/bank_transfer')) {
+		    $this->error['warning'] = $this->language->get('error_permission');
+		}
+		
+		return !$this->error;
+	}
 
 }
